7920b391fa52a31962b670239ed6d23ad98bd13b
Cameron Jenkins <cameron.jenkins@torqit.ca>
2023-03-06 08:13:22 -0400

    allow subfolders, error notify

--- src/Services/Stores/Models/CommitResult.php
+++ src/Services/Stores/Models/CommitResult.php
@@ -4,36 +4,65 @@ namespace TorqIT\StoreSyndicatorBundle\Services\Stores\Models;
 
 use Pimcore\Model\DataObject\Concrete;
 
-class CommitResult{
+class CommitResult
+{
 
     /**
      * @var Concrete[] $updated 
      */
     private array $updated;
-    
+
     /**
      * @var Concrete[] $created 
      */
     private array $created;
 
-    public function __construct(){
+    /** @var String[] $errors */
+    private array $errors;
+
+    public function __construct()
+    {
         $this->updated = array();
         $this->created = array();
     }
 
-    public function addCreated(Concrete $object){
+    public function addCreated(Concrete $object)
+    {
         $this->created[] = $object;
     }
 
-    public function addUpdated(Concrete $object){
+    public function addUpdated(Concrete $object)
+    {
         $this->updated[] = $object;
     }
 
-    public function getCreated(){
+    public function getCreated()
+    {
         return $this->created;
     }
 
-    public function getUpdated(){
+    public function getUpdated()
+    {
         return $this->updated;
+    }
+
+    /**
+     * Set the value of errors
+     *
+     * @param string $error
+     */
+    public function addError(string $error)
+    {
+        $this->errors[] = $error;
+    }
+
+    /**
+     * Get the value of errors
+     *
+     * @return array
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
     }
 }
