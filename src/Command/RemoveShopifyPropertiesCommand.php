33735cdcbe0f05c793801b451d960d0fe7b3d098
Cameron Jenkins <cameron.jenkins@torqit.ca>
2023-03-17 13:25:12 -0300

    way better command to remove properties

--- src/Command/RemoveShopifyPropertiesCommand.php
+++ src/Command/RemoveShopifyPropertiesCommand.php
@@ -6,6 +6,7 @@ use Pimcore\Model\DataObject;
 use Pimcore\Console\AbstractCommand;
 use Pimcore\Model\DataObject\Concrete;
 use Pimcore\Bundle\DataHubBundle\Configuration;
+use Pimcore\Db;
 use Pimcore\Model\Asset;
 use Pimcore\Model\DataObject\Product;
 use Symfony\Component\Console\Input\InputArgument;
@@ -19,66 +20,14 @@ class RemoveShopifyPropertiesCommand extends AbstractCommand
     {
         $this
             ->setName('torq:remove-shopify-properties')
-            ->addArgument('store-name', InputArgument::REQUIRED)
             ->setDescription('Do Shopify Stuff');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $name = $input->getArgument("store-name");
+        $db = Db::get();
 
-        $config = Configuration::getByName($name);
-        $config = $config->getConfiguration();
-
-        $productPaths = $config["products"]["products"];
-        $classType = $config["products"]["class"];
-
-        $classType = "Pimcore\\Model\\DataObject\\" . $classType;
-        foreach ($productPaths as $pathArray) {
-            $path = $pathArray["cpath"];
-            $products = DataObject::getByPath($path);
-            $products = $products->getChildren([DataObject::OBJECT_TYPE_OBJECT, DataObject::OBJECT_TYPE_FOLDER], true);
-            foreach ($products as $product) {
-                $this->recursiveRemove($product, $classType);
-            }
-        }
+        $db->query('Delete from properties where name IN (?, ?)', ['ShopifyProductId', 'ShopifyImageURL']);
         return 0;
-    }
-
-    private function recursiveRemove($dataObject, $classType)
-    {
-        /** @var Concrete $dataObject */
-        if (is_a($dataObject, $classType)) {
-            $dataObject->removeProperty('ShopifyProductId');
-            $this->removeImageProperties($dataObject);
-            $dataObject->save();
-            foreach ($dataObject->getChildren([Concrete::OBJECT_TYPE_VARIANT], true) as $childVariant) {
-                $childVariant->removeProperty('ShopifyProductId');
-                $childVariant->save();
-            }
-        }
-
-        $products = $dataObject->getChildren([DataObject::OBJECT_TYPE_OBJECT, DataObject::OBJECT_TYPE_FOLDER], true);
-
-        foreach ($products as $product) {
-            $this->recursiveRemove($product, $classType);
-        }
-    }
-
-    //this should be generalized somehow
-    public function removeImageProperties(Product $product)
-    {
-        if ($image = $product->getMainImage()) {
-            $images[] = $image;
-        } else {
-            $images = [];
-        }
-        foreach ($product->getImageGallery() as $image) {
-            $images[] = $image;
-        }
-        foreach ($images as $image) {
-            $image->removeProperty("ShopifyImageURL");
-            $image->save();
-        }
     }
 }
