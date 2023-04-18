94e282ba6dfe6ee20e26b099c039edb1fd5693ae
Cameron Jenkins <cameron.jenkins@torqit.ca>
2023-04-18 14:29:41 -0300

    add manual linking command

---
+++ src/Command/LinkShopifyProductsCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace TorqIT\StoreSyndicatorBundle\Command;
+
+use Pimcore\Db;
+use Pimcore\Console\AbstractCommand;
+use Pimcore\Bundle\DataHubBundle\Configuration;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use TorqIT\StoreSyndicatorBundle\Services\ShopifyHelpers\ShopifyProductLinkingService;
+
+class LinkShopifyProductsCommand extends AbstractCommand
+{
+    public function __construct(private ShopifyProductLinkingService $shopifyProductLinkingService)
+    {
+        parent::__construct();
+    }
+    protected function configure()
+    {
+        $this
+            ->setName('torq:link-shopify-products')
+            ->addArgument('store-name', InputArgument::REQUIRED)
+            ->setDescription('Do Shopify Stuff');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        Db::get()->query('SET SESSION wait_timeout = ' . 28800); //timeout to 8 hours for this session
+        $name = $input->getArgument("store-name");
+
+        $config = Configuration::getByName($name);
+        $this->shopifyProductLinkingService->link($config);
+        return 0;
+    }
+}
