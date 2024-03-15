290fe672fc8fbfeb1499ec016d1512018cdcf3bc
Cameron Jenkins <cameron.jenkins@torqit.ca>
2024-03-15 16:31:34 -0300

    change to new routing

--- src/Services/AttributesService.php
+++ src/Services/AttributesService.php
@@ -107,8 +107,8 @@ class AttributesService
     {
         $config = $configuration->getConfiguration();
 
-        $class = $config["products"]["class"];
-        if (!$class = ClassDefinition::getByName($class)) {
+        $class = $config["products"] ?? null ? $config["products"]["class"] : null;
+        if (!$class || !$class = ClassDefinition::getByName($class)) {
             return [];
         }
 
