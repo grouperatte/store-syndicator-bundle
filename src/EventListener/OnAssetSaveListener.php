1326892162d222e44e5a235cf292191f02388089
Arran Fraser <arran.fraser@torqit.ca>
2025-05-30 13:14:33 -0300

    Remove Shopify File Id from Asset anytime it is saved

---
+++ src/EventListener/OnAssetSaveListener.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TorqIT\StoreSyndicatorBundle\EventListener;
+
+
+class OnAssetSaveListener
+{
+    // Assets in Pimcore that have been linked to a Shopify product will have an attribute `shopifyId` set.
+    // This listener will remove the `shopifyId` attribute from the asset when it is saved.
+    // Because the property has been removed, the asset will be re-uploaded to Shopify on the next sync.
+    public function removeShopifyFileId(\Pimcore\Event\Model\AssetEvent $event): void
+    {
+        $asset = $event->getAsset();
+        
+        $asset->removeProperty('shopifyFileId');
+        $asset->removeProperty('shopifyUploadStatus');
+        $asset->removeProperty('shopifyFileStatus');
+    }
+}
